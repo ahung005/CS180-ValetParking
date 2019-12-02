814c1adb17dd1b3035a4e659c3c76235eea2821b
ahung005 <ahung005@ucr.edu>
2019-12-02 12:42:00 -0800

    added Notification

--- parking2/app/src/main/java/com/example/parking/HomePage.java
+++ parking2/app/src/main/java/com/example/parking/HomePage.java
@@ -1,11 +1,20 @@
 package com.example.parking;
 
+import androidx.annotation.RequiresApi;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.Menu;
@@ -18,6 +27,14 @@ import android.widget.Toast;
 
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 public class HomePage extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
@@ -33,15 +50,24 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
     public static TextView textView_lot_32;
     public static TextView textView_lot_50;
 
-//
+    // notification variables
     Handler mHandler;
 
+    private final String CHANNEL_ID = "personal_notification";
+    private final int NOTIFICATION_ID = 001;
+    String day;
+    String time;
+    final DateFormat df = new SimpleDateFormat("HH:mm:ss");
+    String userTime = "12:38:00";
+    //
+
+    @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         this.mHandler = new Handler();
-        m_Runnable.run();
+        m_Runnable.run();   // updates system time for notification
 //
         // Drawer
         mDrawerLayout = findViewById(R.id.drawerLayout);
@@ -69,11 +95,51 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
         setupbuttons();
 
-
-
-
+        Calendar calendar = Calendar.getInstance();     // Notifications
+        day = LocalDate.now().getDayOfWeek().name();    // Notifications
+        time = df.format(Calendar.getInstance().getTime());     // Notifications
 
     }
+
+
+    // Notifications
+    public void sendOnChannel1() {
+        String reminder = "Get your ass to class " + day + " " + time +"\n" ;
+        String lots = "Big Springs: " + textView_big_springs.getText().toString()
+                + "\nLot 6: " + textView_lot_6.getText().toString()
+                + "\nLot 24: " + textView_lot_24.getText().toString()
+                + "\nLot 26: " + textView_lot_26.getText().toString()
+                + "\nLot 30: " + textView_lot_30.getText().toString()
+                + "\nLot 32: " + textView_lot_32.getText().toString()
+                + "\nLot 50: " + textView_lot_50.getText().toString();
+
+        createNotificationChannel();
+
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        builder.setSmallIcon(R.drawable.ic_local_parking_black_24dp);
+        builder.setContentTitle(reminder);
+        builder.setStyle(new NotificationCompat.BigTextStyle().bigText(lots));
+        builder.setPriority(NotificationCompat.PRIORITY_HIGH);
+
+        NotificationManagerCompat notificationManagerCompat = NotificationManagerCompat.from(this);
+        notificationManagerCompat.notify(NOTIFICATION_ID, builder.build());
+    }
+
+    //Notifications
+    private void createNotificationChannel() {
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            CharSequence name = "Personal Notifications";
+            String description = "Inculde all notifications";
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+
+            NotificationChannel notificationChannel = new NotificationChannel(CHANNEL_ID, name, importance);
+            notificationChannel.setDescription(description);
+
+            NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+            notificationManager.createNotificationChannel(notificationChannel);
+        }
+    }
+
 
     private void setupbuttons(){
         Button bigsp = findViewById(R.id.button1);
@@ -95,6 +161,18 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
             }
         });
+
+        textView_big_springs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomePage.this, LotInfo.class);
+                myIntent.putExtra("name", "Big Springs"); //Optional parameters
+                myIntent.putExtra("builds", "Orbach, Physics, Chung, Bourns, Boyce, Webber"); //Optional parameters
+                myIntent.putExtra("maps", "http://maps.google.com/maps?q=loc:33.9753,-117.320114" + " (Big Springs)" );
+                myIntent.putExtra("time", "7 min");
+                HomePage.this.startActivity(myIntent);
+            }
+        });
         lot6.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent myIntent = new Intent(HomePage.this, LotInfo.class);
@@ -105,8 +183,30 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
                 HomePage.this.startActivity(myIntent);
             }
         });
+        textView_lot_6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomePage.this, LotInfo.class);
+                myIntent.putExtra("name", "Lot 6"); //Optional parameters
+                myIntent.putExtra("builds", "Psychology, Olmsted, Humanities, Watkins, Sproul"); //Optional parameters
+                myIntent.putExtra("maps", "http://maps.google.com/maps?q=loc:33.969791,-117.327579" + " (Lot 6)" );
+                myIntent.putExtra("time", "4 min");
+                HomePage.this.startActivity(myIntent);
+            }
+        });
         lot24.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
+                Intent myIntent = new Intent(HomePage.this, LotInfo.class);
+                myIntent.putExtra("name", "Lot 24"); //Optional parameters
+                myIntent.putExtra("builds", "MSE, UNLH, Skye, CHASS, Bourns, Chung"); //Optional parameters
+                myIntent.putExtra("maps", "http://maps.google.com/maps?q=loc:33.978020,-117.330716" + " (Lot  24)" );
+                myIntent.putExtra("time", "6 min");
+                HomePage.this.startActivity(myIntent);
+            }
+        });
+        textView_lot_24.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
                 Intent myIntent = new Intent(HomePage.this, LotInfo.class);
                 myIntent.putExtra("name", "Lot 24"); //Optional parameters
                 myIntent.putExtra("builds", "MSE, UNLH, Skye, CHASS, Bourns, Chung"); //Optional parameters
@@ -126,6 +226,17 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
             }
         });
+        textView_lot_26.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomePage.this, LotInfo.class);
+                myIntent.putExtra("name", "Lot 26"); //Optional parameters
+                myIntent.putExtra("builds", "MSE, UNLH, Skye, CHASS, Bourns, Chung");
+                myIntent.putExtra("maps", "http://maps.google.com/maps?q=loc:33.981198,-117.334054" + " (Lot 26)" );
+                myIntent.putExtra("time", "18 min");
+                HomePage.this.startActivity(myIntent);
+            }
+        });
         lot30.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent myIntent = new Intent(HomePage.this, LotInfo.class);
@@ -135,6 +246,17 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
                 myIntent.putExtra("time", "10 min");
                 HomePage.this.startActivity(myIntent);
 
+            }
+        });
+        textView_lot_30.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomePage.this, LotInfo.class);
+                myIntent.putExtra("name", "Lot 30"); //Optional parameters
+                myIntent.putExtra("builds", "Olmsted, Humanities, Watkins, H&SS");
+                myIntent.putExtra("maps", "http://maps.google.com/maps?q=loc:33.970508,-117.331924" + " (Lot 30)" );
+                myIntent.putExtra("time", "10 min");
+                HomePage.this.startActivity(myIntent);
             }
         });
         lot32.setOnClickListener(new View.OnClickListener(){
@@ -148,6 +270,17 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
             }
         });
+        textView_lot_32.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomePage.this, LotInfo.class);
+                myIntent.putExtra("name", "Lot 32"); //Optional parameters
+                myIntent.putExtra("builds", "Olmsted, Humanities, Watkins, H&SS");
+                myIntent.putExtra("maps", "http://maps.google.com/maps?q=loc:33.968962,-117.330248" + " (Lot 32)" );
+                myIntent.putExtra("time", "11 min");
+                HomePage.this.startActivity(myIntent);
+            }
+        });
         lot50.setOnClickListener(new View.OnClickListener(){
             public void onClick(View v){
                 Intent myIntent = new Intent(HomePage.this, LotInfo.class);
@@ -159,15 +292,33 @@ public class HomePage extends AppCompatActivity implements NavigationView.OnNavi
 
             }
         });
+        textView_lot_50.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomePage.this, LotInfo.class);
+                myIntent.putExtra("name", "Lot 50"); //Optional parameters
+                myIntent.putExtra("builds", "Arts, CHASS, UNLH, H&SS, Sproul");
+                myIntent.putExtra("maps", "http://maps.google.com/maps?q=loc:33.974956,-117.336570" + " (Lot 50)" );
+                myIntent.putExtra("time", "14 min");
+                HomePage.this.startActivity(myIntent);
+            }
+        });
 
         return;
     }
 
+    // updates notification time and parking spaces
     private final Runnable m_Runnable = new Runnable() {
         public void run() {
+            time = df.format(Calendar.getInstance().getTime());         //
+            if(time.equals(userTime)) {                                  //
+                sendOnChannel1();                                       //
+            }
+
+
             com.example.parking.fetchData process = new com.example.parking.fetchData();
             process.execute();
-            HomePage.this.mHandler.postDelayed(m_Runnable,60000);
+            HomePage.this.mHandler.postDelayed(m_Runnable,1000);
         }
 
     };
